10cecd7c5201344be11ea049c50c103ab874aaa8
stephan-visser <63516378+stephan-visser@users.noreply.github.com>
2020-04-22 12:53:04 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -22,6 +22,46 @@
         <li><a href="logout.php">Uitloggen</a></li>
     </ul>
 </nav>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="hbtext">
+                    <h1>Welkom!</h1>
+                    <p class="welkomtext">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia laborum, illum velit aspernatur dolore odio, blanditiis excepturi, suscipit et fugit iste. Blanditiis sequi eum nobis libero? Fugiat voluptas illo temporibus pariatur eaque assumenda doloribus? Maiores consequuntur molestias libero dolor in sit dicta perferendis dolores adipisci labore. Hic, pariatur atque ipsam accusamus reiciendis officiis voluptates perspiciatis error corporis delectus similique magni quas animi quasi earum aut sint doloremque nihil quia blanditiis!</p>
+                </div>
+                <div class="categorieen">
+                    <h2 >Categorieën</h2>
+                    <p>•Lorem ipsum dolor sit amet.</p>
+                    <p>•Lorem ipsum dolor sit amet consectetur.</p>
+                    <p>•Lorem ipsum dolor sit.</p>
+                    <p>•Lorem ipsum dolor sit amet.</p>
+                </div>
+                <div>
+                    <h2>Misschien iets voor u.</h2>
+                    <a>•Lorem ipsum dolor sit.</a><br>
+                    <a>•Lorem, ipsum dolor.</a><br>
+                    <a>•Lorem ipsum dolor sit amet.</a><br>
+                    <a>•Lorem ipsum dolor sit amet consectetur.</a><br>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="search-bar">
+                    <form action="#">
+                        <input type="text" class="input-md" placeholder="Zoeken..">
+                        <button class="btn btn-md btn-primary glyphicon glyphicon-search" type="button">
+                            <i class="fa fa-search"></i>
+                        </button>
+                    </form>
+                </div>
+                <div class="laatstbekeken">
+                    <br>
+                    <h2>Laatst bekeken</h2>
+                    <p>•Lorem ipsum dolor sit amet.</p>
+                    <p>•Lorem, ipsum dolor.</p>
+                    <p>•Lorem ipsum dolor sit.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
