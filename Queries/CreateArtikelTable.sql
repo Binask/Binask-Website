b7224b7cd022651c3119ce1552582ba7150e2b5f
w-hulshoff <55273247+w-hulshoff@users.noreply.github.com>
2020-04-27 16:18:28 +0200

    Add files via upload

--- Queries/CreateArtikelTable.sql
+++ Queries/CreateArtikelTable.sql
@@ -1,7 +1,8 @@
 CREATE TABLE Artikelen (
-    id int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    ArtikelTekst mediumtext NOT NULL,
-    ArtikelTitel mediumtext NOT NULL,
-    ArtikelSamenvatting mediumtext NOT NULL,
-    ArtikelTags mediumtext NOT NULL
-)
+                            id int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                            ArtikelTitel mediumtext NOT NULL,
+                            ArtikelSamenvatting mediumtext NOT NULL,
+                            ArtikelLink mediumtext NOT NULL,
+                            ArtikelCategorie mediumtext NOT NULL,
+                            ArtikelRank int(4) DEFAULT '9999'
+);
