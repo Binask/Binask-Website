0eeceac2928b00776820f2893954cc8d2460a994
stephan-visser <63516378+stephan-visser@users.noreply.github.com>
2020-05-12 08:29:49 +0200

    session fixed

--- session.php
+++ session.php
@@ -6,4 +6,7 @@ $user_check = $_SESSION['user_id'];
 $user_role = $_SESSION['user_type'];
 $user_firstname = $_SESSION["user_firstname"];
 
+if(!isset($_SESSION['user_login'])){
+    header("location:login.php");
+}
 ?>
