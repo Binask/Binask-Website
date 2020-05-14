3e188f979683b5334645a1a43938b5f6bf4d22c6
w-hulshoff <55273247+w-hulshoff@users.noreply.github.com>
2020-05-14 13:59:27 +0200

    Add files via upload

--- Queries/CreateArtikelTable.sql
+++ Queries/CreateArtikelTable.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Artikelen (
-                            id int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                            ArtikelTitel mediumtext NOT NULL,
-                            ArtikelSamenvatting mediumtext NOT NULL,
-                            ArtikelLink mediumtext NOT NULL,
-                            ArtikelCategorie mediumtext NOT NULL,
-                            ArtikelRank int(4) DEFAULT '9999'
+                           id int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+                           ArtikelTitel mediumtext NOT NULL,
+                           ArtikelSamenvatting mediumtext NOT NULL,
+                           ArtikelLink mediumtext NOT NULL,
+                           ArtikelCategorie mediumtext NOT NULL,
+                           ArtikelRank int(11) NOT NULL
 );
--- Queries/InsertTestartikelIntoArtikelen.sql
+++ Queries/InsertTestartikelIntoArtikelen.sql
@@ -1,2 +1,2 @@
 INSERT INTO Artikelen (ArtikelTitel, ArtikelSamenvatting, ArtikelLink ,ArtikelCategorie, ArtikelRank)
-VALUES ('titel', 'samenvatting', 'www.test.com', 'test', '1')
+VALUES ('titel', 'samenvatting', 'https://www.google.com', 'test', '1')
